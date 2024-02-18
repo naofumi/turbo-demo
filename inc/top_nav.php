192b8ea2fa18fee730a0dd4a38587716068c005a
Naofumi Kagami <naofumi@mac.com>
2024-02-18 23:22:15 +0900

    Consolidate sleep.php

--- inc/top_nav.php
+++ inc/top_nav.php
@@ -1,4 +1,4 @@
 <nav style="width: 100%; height: 32px; background: gray; text-align: center;padding: 4px;">
     <a href="index.php" style="color: white;">Top</a>
-    <a href="sleep.php" style="color: white;">Sleep <?php echo $_SESSION['sleep_duration'] ?></a>
+    <a href="../sleep/sleep.php" style="color: white;">Sleep <?php echo $_SESSION['sleep_duration'] ?></a>
 </nav>
