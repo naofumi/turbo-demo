180f3d55f5415d3c1e02e62507b7103d8bd3c92a
Naofumi Kagami <naofumi@mac.com>
2024-02-18 12:23:43 +0900

    Remove morph stuff from MPA

--- inc/head-mpa.php
+++ inc/head-mpa.php
@@ -3,6 +3,4 @@
     <title>Turbo 8 morphing demo: Index</title>
     <link rel="stylesheet" href="../css/style.css" />
     <script src="../js/turbo.js" data-turbo-track="reload"></script>
-    <meta name="turbo-refresh-scroll" content="preserve">
-    <meta name="turbo-refresh-method" content="morph">
 </head>
