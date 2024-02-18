58df76932de5542af33566ea60d360dd95b707d8
Naofumi Kagami <naofumi@mac.com>
2024-02-18 18:11:28 +0900

    Allow for fractional sleep seconds

--- inc/start.php
+++ inc/start.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset($_SESSION['sleep_duration'])) {
-    sleep($_SESSION['sleep_duration']);
+    usleep($_SESSION['sleep_duration'] * 1000000);
 } else {
     $_SESSION['sleep_duration'] = 0;
 }
