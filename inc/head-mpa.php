77261107557bf0b292350719a310d201fbb000d0
Naofumi Kagami <naofumi@mac.com>
2024-02-23 15:40:56 +0900

    Fix title

--- inc/head-mpa.php
+++ inc/head-mpa.php
@@ -1,5 +1,5 @@
 <head>
     <meta charset="UTF-8">
-    <title>Turbo 8 morphing demo: Index</title>
+    <title>Turbo demo</title>
     <link rel="stylesheet" href="../css/style.css"/>
 </head>
--- inc/head.php
+++ inc/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <title>Turbo 8 morphing demo: Index</title>
+    <title>Turbo demo</title>
     <link rel="stylesheet" href="../css/style.css" />
     <script src="../js/turbo.js" data-turbo-track="reload"></script>
     <script src="../js/utilities.js" data-turbo-track="reload"></script>
